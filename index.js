3b6831458538894f5e96b0146f836925a5c605b9
Abigail Arisa <abigailarisa1@gmail.com>
2022-08-29 22:40:47 -0500

    commit tree 7c7e2219fdca86e4a8b4069e7a36c87c1e846036

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser ='Grace Hopper';
+const welcomeMessage = "Welcome to Flatbook, currentUser + 'Grace Hopper'!";
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, currentUser + 'GRACE HOPPER'!";
+const shortGreeting =   "Welcome, + 'G'!";
